98154097efb1bbd217e613b28c28581d072ef406
johary1 <joharisoa@gmail.com>
2023-03-11 15:35:31 +0100

    add filter parts and some updates

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
-const result = document.getElementById("result");
-const form = document.querySelector("form");
-const input = document.querySelector("input");
+let recipes;
 
-let recipes = [];
-async function fetchRecipes(search) {
-  let url = "data/recipes.json";
-  // generate the Response object
-  await fetch(`${url}?s=${search}`)
-    .then((res) => res.json())
-    .then((data) => (recipes = data.recipes));
-  console.log(recipes);
+async function getDataJson() {
+  const response = await fetch("data/recipes.json");
+  recipes = (await response.json()).recipes;
+  init();
 }
-console.log("start filter");
-console.log(fetchRecipes(search));
-console.log("end filter");
 
-function recipesDisplay() {
-  for (var i = 0; i < recipes.length; i++) {
-    if (recipes === null) {
-      result.innerHTML = "<h2>Aucun résultat</h2>";
-    } else {
-      result.innerHTML = recipes
-        .map((recipe) => {
-          for (var i = 0; i < recipe.ingredients.length; i++) {
-            var ingredient = recipe.ingredients[i];
-            var ingredientString = ingredient.ingredient;
-            if (ingredient.quantity) {
-              ingredientString += " : " + ingredient.quantity;
-            }
-            if (ingredient.unit) {
-              ingredientString += " " + ingredient.unit;
-            }
-            console.log(ingredientString);
-          }
-          return `
-          <li class="card">
-          <img src="./assets/images/image.jpg" alt="photo" width="250" height="150">
-          <h2>${recipe.name} - ${recipe.time}</h2>
-          <p>${recipe.description}</p>
-          <ul>${ingredientString}</ul>
-          
-          
-          </li>
-          `;
-        })
-        .join("");
-    }
+/* option 1 */
+function displayData(recipes) {
+  const recipeSection = document.getElementById("recipes__cards");
+  recipeSection.innerHTML = "";
+  // getRecipeCard is defined in recipeCards.js
+  recipes.forEach((recipe) => {
+    const recipeCard = getRecipeCard(recipe);
+    recipeSection.appendChild(recipeCard);
+  });
+}
+
+/* option 2  
+function displayData(recipes) {
+  const recipeSection = document.getElementById("recipes__cards");
+  recipeSection.innerHTML = "";
+  for (const recipe of recipes) {
+    //getRecipeCard is defined in recipeCards.js 
+    const recipeCard = getRecipeCard(recipe);
+    recipeSection.appendChild(recipeCard);
   }
 }
-input.addEventListener("input", (e) => {
-  fetchRecipes(e.target.value);
-});
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  recipesDisplay();
-  fetchRecipes(e.target.value);
-});
+*/
+
+function init() {
+  /* Display all recipes */
+  console.time("display recipe");
+  displayData(recipes);
+  console.timeEnd("display recipe");
+  /* Display all filters */
+  filterIngredients();
+  filterAppliances();
+  filterUstensils();
+  /* Display filter's item on click */
+  isArrowClicked();
+  /* Update filter item content with recipes from search input */
+  fillFilters(recipes);
+}
+
+getDataJson();
+/* Reset search and filters */
+document
+  .querySelector(".fa-arrow-rotate-right")
+  .addEventListener("click", function () {
+    window.location.reload();
+  });
---
+++ script/utils/mainSearch.js
@@ -0,0 +1,92 @@
+/** recipesToDisplay is defined in filterTags.js **/
+/** filteredRecipesWithTags is defined in tags.js **/
+/** fillFilters is defined in filterUpdate.js **/
+/** DisplayData is defined in index.js **/
+
+const searchBarInput = document.querySelector(".search__input");
+const noResultText = document.querySelector(".not-found");
+
+/** Manage search with filters  **/
+function realtimeSearch() {
+  let tagsUsed = false;
+  recipesToDisplay = [];
+  let mainInput;
+
+  // Return array with filtered recipes
+  if (searchBarInput.value.length > 2) {
+    mainInput = searchBarInput.value;
+
+    const regex = new RegExp(`${mainInput.trim().toLowerCase()}`);
+    recipesToDisplay = recipes.filter((recipe) => {
+      let recipeIsMatching = false;
+      if (regex.test(recipe.name)) {
+        recipeIsMatching = true;
+      } else if (regex.test(recipe.description)) {
+        recipeIsMatching = true;
+      }
+      recipe.ingredients.forEach(({ ingredient }) => {
+        if (regex.test(ingredient)) {
+          recipeIsMatching = true;
+        }
+      });
+      return recipeIsMatching;
+    });
+
+    fillFilters(recipesToDisplay);
+  }
+
+  if (
+    Array.from(
+      document.querySelectorAll(
+        ".tag__ingredients--wrapper .tag__ingredient .tag-blue"
+      )
+    ).length > 0 ||
+    Array.from(
+      document.querySelectorAll(
+        ".tag__appliances--wrapper .tag__appliance .tag-green"
+      )
+    ).length > 0 ||
+    Array.from(
+      document.querySelectorAll(
+        ".tag__ustensils--wrapper .tag__ustensil .tag-red"
+      )
+    ).length > 0
+  ) {
+    tagsUsed = true;
+    if (recipesToDisplay.length > 0) {
+      recipesToDisplay = filteredRecipesWithTags(recipesToDisplay);
+    } else {
+      recipesToDisplay = filteredRecipesWithTags(recipes);
+    }
+  }
+
+  // handle case when there is no result
+  if (recipesToDisplay.length > 0) {
+    noResultText.innerHTML = "";
+    displayData(recipesToDisplay);
+  } else {
+    displayData(recipesToDisplay);
+    noResultText.innerHTML = "<p>Aucun résultat correspondant ...</p>";
+  }
+
+  // else
+  if (
+    (searchBarInput.value === "" || searchBarInput.value.length < 3) &&
+    tagsUsed === false
+  ) {
+    fillFilters(recipes);
+    displayData(recipes);
+    noResultText.innerHTML = "";
+  }
+}
+
+/** using a timer to delay the execution of the realtimeSearch()  until the user has finished typing**/
+let userTimerType;
+
+const typeInterval = 100;
+searchBarInput.addEventListener("keyup", () => {
+  clearTimeout(userTimerType);
+  userTimerType = setTimeout(() => {
+    realtimeSearch();
+  }, typeInterval);
+});
